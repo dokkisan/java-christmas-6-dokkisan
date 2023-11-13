2496f5b81d8736a1f29d0570562167f4c82dd02a
dokkisan <dokkisan.wang@gmail.com>
2023-11-13 20:59:10 +0900

    feat(InputView): 예상 주문 메뉴 입력 기능 구현

--- src/main/java/christmas/model/ErrorMessage.java
+++ src/main/java/christmas/model/ErrorMessage.java
@@ -1,8 +1,8 @@
 package christmas.model;
 
 public enum ErrorMessage {
-    INVALID_NUMBER_FORMAT("숫자만 입력해 주세요. (문자, 기호, 공백은 입력 불가합니다.)"),
-    INVALID_DAYS_IN_DECEMBER("이벤트 혜택 기간은 12월 1일~12월 31일까지 입니다. 이벤트 기간 내 일자를 입력해 주세요.");
+    INVALID_DAYS_IN_DECEMBER("유효하지 않은 날짜입니다. 다시 입력해 주세요."),
+    INVALID_MENU_FORMAT("유효하지 않은 주문입니다. 다시 입력해 주세요.");
 
     private final String message;
 
--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -3,17 +3,51 @@ package christmas.view;
 import camp.nextstep.edu.missionutils.Console;
 import christmas.model.ErrorMessage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputView {
+    public static final int INDEX_OF_MENU_NAME = 0;
+    public static final int INDEX_OF_MENU_QUANTITY = 0;
 
     public int askExpectedVisitDate() {
-        return parseInt(Console.readLine());
+        return parseInt(Console.readLine(), true);
     }
 
-    private int parseInt(String input) {
+    public List<String> askExpectedMenuItems() {
+        return validate(Console.readLine());
+    }
+
+    private int parseInt(String input, boolean isExpectedVisitDate) {
         try {
-            return Integer.parseInt(input);
+            int number = Integer.parseInt(input);
+            if (number < 0) {
+                throw new IllegalArgumentException(ErrorMessage.INVALID_MENU_FORMAT.getMessage());
+            }
+            return number;
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(ErrorMessage.INVALID_NUMBER_FORMAT.getMessage());
+            if (isExpectedVisitDate) {
+                throw new IllegalArgumentException(ErrorMessage.INVALID_DAYS_IN_DECEMBER.getMessage());
+            }
+            throw new IllegalArgumentException(ErrorMessage.INVALID_MENU_FORMAT.getMessage());
         }
+    }
+
+    private List<String> validate(String inputExpectedMenuItems) {
+        final String COMMA = ",";
+        final String HYPHEN = "-";
+        List<String> menuItems = new ArrayList<>();
+
+        for (String item : inputExpectedMenuItems.split(COMMA)) {
+            String[] parts = item.split(HYPHEN);
+            if (parts.length != 2) {
+                throw new IllegalArgumentException(ErrorMessage.INVALID_MENU_FORMAT.getMessage());
+            }
+            int quantity = parseInt(parts[INDEX_OF_MENU_QUANTITY], false);
+            menuItems.add(parts[INDEX_OF_MENU_NAME]);
+            menuItems.add(String.valueOf(quantity));
+        }
+
+        return menuItems;
     }
 }
